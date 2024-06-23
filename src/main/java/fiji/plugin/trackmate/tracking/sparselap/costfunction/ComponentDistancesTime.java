317b237611917736c6cfd9ee86399032a2063dd8
Pierre Parutto <pvp23@cam.ac.uk>
2024-06-23 16:34:55 +0100

    Clean struct aware code

--- src/main/java/fiji/plugin/trackmate/tracking/sparselap/costfunction/ComponentDistancesTime.java
+++ src/main/java/fiji/plugin/trackmate/tracking/sparselap/costfunction/ComponentDistancesTime.java
@@ -6,6 +6,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Objects;
 
@@ -17,36 +19,22 @@ import ij.ImagePlus;
 
 public class ComponentDistancesTime
 {
-	public class PixelGroupTime
-	{
-		public int px3D;
-		public int win_start;
-		public int win_end;
-
-		public PixelGroupTime( int px3D, int w_start, int w_end )
-		{
-			this.px3D = px3D;
-			this.win_start = w_start;
-			this.win_end = w_end;
-		}
-	}
-
 	public class CompTime
 	{
 		public float dist;
-		public int frameStart;
-		public int frameEnd;
+		public int winStart;
+		public int winEnd;
 
-		public CompTime( float dist, int frameStart, int frameEnd )
+		public CompTime( float dist, int winStart, int winEnd )
 		{
 			this.dist = dist;
-			this.frameStart = frameStart;
-			this.frameEnd = frameEnd;
+			this.winStart = winStart;
+			this.winEnd = winEnd;
 		}
 
-		public boolean inInterval( int frame )
+		public boolean inInterval( int win )
 		{
-			return frame >= this.frameStart && frame <= this.frameEnd;
+			return win >= this.winStart && win <= this.winEnd;
 		}
 
 		@Override
@@ -54,22 +42,22 @@ public class ComponentDistancesTime
 		{
 			final int prime = 31;
 			int result = 1;
-			result = prime * result + Objects.hash(dist, frameEnd, frameStart);
+			result = prime * result + Objects.hash( dist, winEnd, winStart );
 			return result;
 		}
 
 		@Override
-		public boolean equals(Object obj)
+		public boolean equals( Object obj )
 		{
-			if (this == obj)
+			if ( this == obj )
 				return true;
-			if (obj == null)
+			if ( obj == null )
 				return false;
-			if (getClass() != obj.getClass())
+			if ( getClass() != obj.getClass() )
 				return false;
-			CompTime other = (CompTime) obj;
-			return Float.floatToIntBits(dist) == Float.floatToIntBits(other.dist) && frameEnd == other.frameEnd
-					&& frameStart == other.frameStart;
+			CompTime other = ( CompTime ) obj;
+			return Float.floatToIntBits( dist ) == Float.floatToIntBits( other.dist ) && winEnd == other.winEnd
+					&& winStart == other.winStart;
 		}
 	}
 
@@ -85,11 +73,102 @@ public class ComponentDistancesTime
 		}
 	}
 
+	public class Distances
+	{
+		public int[] comps;
+		public int[][] pxSrc;
+		public int[][][] pxDst;
+		public int[][][][] dists;
+
+		public Distances()
+		{
+			this.comps = null;
+			this.pxSrc = null;
+			this.pxDst = null;
+			this.dists = null;
+		}
+
+		public void initComps( int n )
+		{
+			this.comps = new int[n];
+			this.pxSrc = new int[n][];
+			this.pxDst = new int[n][][];
+			this.dists = new int[n][][][];
+		}
+
+		public void initSrcs( int cidx, int n )
+		{
+			this.pxSrc[cidx] = new int[n];
+			this.pxDst[cidx] = new int[n][];
+			this.dists[cidx] = new int[n][][];
+		}
+
+		public void initDsts( int cidx, int srcidx, int n )
+		{
+			this.pxDst[cidx][srcidx] = new int[n];
+			this.dists[cidx][srcidx] = new int[n][];
+		}
+
+		public int getCompIdx( int comp )
+		{
+			return Arrays.binarySearch( this.comps, comp );
+		}
+
+		public int getCompSrcIdx( int cIdx, int px )
+		{
+			return Arrays.binarySearch( this.pxSrc[cIdx], px );
+		}
+
+		public int getCompDstIdx( int cIdx, int pxSrcIdx, int pxDst )
+		{
+			return Arrays.binarySearch( this.pxDst[cIdx][pxSrcIdx], pxDst );
+		}
+
+		public int getDist( int cIdx, int pxSrcIdx, int pxDstIdx, int winIdx )
+		{
+			if ( cIdx >= this.dists.length || pxSrcIdx >= this.dists[cIdx].length || pxDstIdx >= this.dists[cIdx][pxSrcIdx].length || winIdx >= this.dists[cIdx][pxSrcIdx][pxDstIdx].length )
+				assert(false);
+			return this.dists[cIdx][pxSrcIdx][pxDstIdx][winIdx];
+		}
+	}
+
+	public static class ArrCmp implements Comparator<Integer>
+	{
+		private final int[] arr;
+
+		public ArrCmp ( final int[] arr )
+		{
+			this.arr = arr;
+		}
+
+		@Override
+		public int compare( Integer i, Integer j )
+		{
+			return Integer.compare( this.arr[i], this.arr[j] );
+		}
+	}
+
+	public static <T> void sort_simultaneous( int[] arr1, T[] arr2 )
+	{
+		Integer[] idxs = new Integer[arr1.length];
+		for ( int u = 0; u < arr1.length; ++u )
+			idxs[u] = u;
+		Arrays.sort( idxs, new ArrCmp( arr1 ) );
+		int[] tmp1 = new int[arr1.length];
+		ArrayList<T> tmp2 = new ArrayList<> ( arr1.length );
+		for ( int u = 0; u < arr1.length; ++u )
+		{
+			tmp1[u] = arr1[idxs[u]];
+			tmp2.add( arr2[idxs[u]] );
+		}
+		arr1 = tmp1;
+		tmp2.toArray( arr2 );
+	}
+
 	protected HashMap<CompTime, Integer> elts_f;
-	protected HashMap<Integer, CompTime> elts_r;
-	protected IntHashMap< IntHashMap< IntHashMap< ArrayList< Integer > > > > dists;
-	protected IntHashMap< ArrayList< PixelGroupTime > > px1D_to_3D;
-	protected ArrayList<Integer> win_cents;
+	protected ArrayList<CompTime> elts_r;
+	Distances dists;
+	protected ArrayList<Integer> win_starts;
 	protected boolean is2D;
 	protected int wdur;
 	protected int w;
@@ -101,6 +180,15 @@ public class ComponentDistancesTime
 	protected static int[][] nh_idxs = { { -1, -1 }, { -1, 0 }, { -1, 1 },
 										 { 0,  -1 },            {  0, 1 },
 										 { 1,  -1 }, {  1, 0 }, {  1, 1 } };
+
+	public int getWinIdx( int cIdx, int pxSrcIdx, int pxDstIdx, int win )
+	{
+		int[] eltIdxs = this.dists.dists[cIdx][pxSrcIdx][pxDstIdx];
+		for ( int i = 0; i < eltIdxs.length; ++i )
+			if ( this.elts_r.get( eltIdxs[i] ).inInterval( win ) )
+				return i;
+		return -1;
+	}
 
 	protected static int byte_to_int( final byte[] bs )
 	{
@@ -121,19 +209,6 @@ public class ComponentDistancesTime
 			   Math.pow( pos[1] - ( p[1] * pxsize + pxsize / 2 ), 2 );
 	}
 
-	public int map_1D_to_3D( int px1D, int win )
-	{
-		//Change to use win later
-		if ( !this.px1D_to_3D.containsKey( px1D ) )
-			return -1;
-
-		for ( PixelGroupTime ct: this.px1D_to_3D.get( px1D ) )
-			if ( ct.win_start >= win && ct.win_end <= win )
-				return ct.px3D;
-
-		return -1;
-	}
-
 	protected float[] read_dists( FileInputStream is ) throws IOException
 	{
 		byte[] buff_i = new byte[3];
@@ -151,59 +226,139 @@ public class ComponentDistancesTime
 		return res;
 	}
 
-	protected void read_binary_spts( final String fname, double dt ) throws FileNotFoundException
+	protected void read_binary_spts( final String fname ) throws FileNotFoundException
 	{
 		FileInputStream is = new FileInputStream( fname );
 		byte[] buff_i = new byte[3];
 
 		float[] all_dists = null;
-		this.win_cents = new ArrayList<> ();
+		this.win_starts = new ArrayList<> ();
 
 		try
 		{
 			is.read( buff_i );
 			int version = byte_to_int( buff_i );
+			System.out.println(String.format("Version=%d", version));
 
-			if ( version == 2 ) //this is 2D data
+			if ( version == 1 ) //this is 2D+t data
 			{
-				this.is2D = true;
-				all_dists = read_dists( is );
+				byte[] buff_f = new byte[4];
 
-				this.px1D_to_3D = new IntHashMap< ArrayList<PixelGroupTime> > ();
+				this.is2D = false;
+
+				is.read( buff_i );
+				int Wdur = byte_to_int( buff_i );
+				this.wdur = Wdur;
+
+				is.read( buff_f );
+				float Wover = byte_to_float( buff_f );
+
+				is.read( buff_i );
+				int Wmax = byte_to_int( buff_i );
+
+				System.out.println( String.format( "%d %f %d", Wdur, Wover, Wmax ) );
+
+				int dw = ( int ) Math.floor( Wdur * ( 1 - Wover ) );
+				if ( Wover == 0 )
+					dw = Wdur;
+
+				this.win_starts.add( 0 );
+				for ( int i = 1; i < Wmax; ++i )
+					this.win_starts.add( ( dw + 1 ) * i );
+
+				String s = "";
+				for ( int i = 0; i < this.win_starts.size() ; ++i )
+					s = s.concat( String.format( " [%d, %d]", this.win_starts.get( i ), this.win_starts.get( i ) + Wdur -1 ) );
+				System.out.println( String.format( "Time windows (%d): %s", this.win_starts.size(), s ) );
+
+				all_dists = read_dists( is );
 
 				is.read( buff_i );
 				int Ncomps = byte_to_int( buff_i );
+				this.dists.initComps( Ncomps );
 				for ( int i = 0; i < Ncomps; ++i )
 				{
 					is.read( buff_i );
 					int comp = byte_to_int( buff_i );
-					this.dists.put( comp, new IntHashMap<> () );
+					this.dists.comps[i] = comp;
 
 					is.read( buff_i );
 					int Npxs1 = byte_to_int( buff_i );
+					this.dists.initSrcs( i, Npxs1 );
 					for ( int j = 0; j < Npxs1; ++j )
 					{
 						is.read( buff_i );
 						int px1 = byte_to_int( buff_i );
-						this.dists.get( comp ).put( px1, new IntHashMap<> ( 2 ) );
-						if ( !this.px1D_to_3D.containsKey( px1 ) )
-						{
-							this.px1D_to_3D.put( px1 , new ArrayList<> () );
-							this.px1D_to_3D.get( px1 ).add( new PixelGroupTime( px1, 0, 0 ) );
-						}
+						this.dists.pxSrc[i][j] = px1;
 
 						is.read( buff_i );
 						int Npxs2 = byte_to_int( buff_i );
+						this.dists.initDsts( i, j, Npxs2 );
 						for ( int k = 0; k < Npxs2; ++k )
 						{
 							is.read( buff_i );
 							int px2 = byte_to_int( buff_i );
+							this.dists.pxDst[i][j][k] = px2;
 
-							if ( !this.px1D_to_3D.containsKey( px2 ) )
+							is.read( buff_i );
+							int Nwins = byte_to_int( buff_i );
+							this.dists.dists[i][j][k] = new int[Nwins];
+							for ( int l = 0; l < Nwins; ++l )
 							{
-								this.px1D_to_3D.put( px2 , new ArrayList<> () );
-								this.px1D_to_3D.get( px2 ).add( new PixelGroupTime( px2, 0, 0 ) );
+								is.read( buff_i );
+								int didx = byte_to_int( buff_i );
+
+								is.read( buff_i );
+								int wstart = byte_to_int( buff_i );
+
+								is.read( buff_i );
+								int wend = byte_to_int( buff_i );
+
+								CompTime ct = new CompTime( all_dists[didx], wstart, wend );
+								if ( ! this.elts_f.containsKey( ct ) )
+								{
+									int N = this.elts_f.size();
+									this.elts_f.put( ct, N );
+									this.elts_r.add( ct );
+								}
+
+								this.dists.dists[i][j][k][l] = this.elts_f.get( ct );
 							}
+						}
+					}
+				}
+			}
+			else if ( version == 2 ) //this is 2D data
+			{
+				this.is2D = true;
+				all_dists = read_dists( is );
+
+				is.read( buff_i );
+				int Ncomps = byte_to_int( buff_i );
+				this.dists.initComps( Ncomps );
+				for ( int i = 0; i < Ncomps; ++i )
+				{
+					is.read( buff_i );
+					int comp = byte_to_int( buff_i );
+					this.dists.comps[i] = comp;
+
+					is.read( buff_i );
+					int Npxs1 = byte_to_int( buff_i );
+					this.dists.initSrcs( i, Npxs1 );
+					for ( int j = 0; j < Npxs1; ++j )
+					{
+						is.read( buff_i );
+						int px1 = byte_to_int( buff_i );
+						this.dists.pxSrc[i][j] = px1;
+
+						is.read( buff_i );
+						int Npxs2 = byte_to_int( buff_i );
+						this.dists.initDsts( i, j, Npxs2 );
+						for ( int k = 0; k < Npxs2; ++k )
+						{
+							is.read( buff_i );
+							int px2 = byte_to_int( buff_i );
+							this.dists.pxDst[i][j][k] = px2;
 
 							is.read( buff_i );
 							int didx = byte_to_int( buff_i );
@@ -213,116 +368,16 @@ public class ComponentDistancesTime
 							{
 								int N = this.elts_f.size();
 								this.elts_f.put( ct, N );
-								this.elts_r.put( N, ct );
+								this.elts_r.add( ct );
 							}
 
-							this.dists.get( comp ).get( px1 ).put( px2, new ArrayList<> () );
-							this.dists.get( comp ).get( px1 ).get( px2 ).add( elts_f.get( ct ) );
+							this.dists.dists[i][j][k] = new int[] { this.elts_f.get( ct ) };
 						}
 					}
 				}
 			}
 			else
 				assert( false );
-
-//			is.read( buff_i );
-//			int Wdur = byte_to_int( buff_i );
-//			this.wdur = Wdur;
-//
-//			is.read( buff_f );
-//			float Wover = byte_to_float( buff_f );
-//
-//			is.read( buff_i );
-//			int Wmax = byte_to_int( buff_i );
-//
-//			//System.out.println(String.format("%d %f %d", Wdur, Wover, Wmax));
-//
-//			int dw = ( int ) Math.floor( Wdur * Wover );
-//			if ( Wover == 0 )
-//				dw = ( int ) Math.floor( ( ( float ) Wdur ) / 2.0 );
-//
-//			for ( int i = 1; i <= Wmax; ++i )
-//				this.win_cents.add( i * dw );
-//
-//			String s = "";
-//			for ( int i = 0; i < this.win_cents.size() ; ++i )
-//				s = s.concat( String.format( " %d [%d, %d]", this.win_cents.get ( i ), this.win_cents.get( i ) - dw, this.win_cents.get( i ) + dw ) );
-//			System.out.println( String.format( "Time windows (%d): %s", this.win_cents.size(), s ) );
-//
-//			//System.out.println(String.format("%d %g %d %d %d", Wdur, Wover, Wmax, dw, this.win_cents.size()));
-//
-//			is.read( buff_i );
-//			int Nelt = byte_to_int( buff_i );
-//			this.px1D_to_3D = new IntHashMap< ArrayList<PixelGroupTime> > ();
-//			for ( int i = 0; i < Nelt; ++i )
-//			{
-//				is.read( buff_i );
-//				int px1D = byte_to_int( buff_i );
-//				is.read( buff_i );
-//				int wStart = byte_to_int( buff_i );
-//				is.read( buff_i );
-//				int wEnd = byte_to_int( buff_i );
-//				if ( !this.px1D_to_3D.containsKey( px1D ) )
-//					this.px1D_to_3D.put( px1D, new ArrayList<PixelGroupTime> () );
-//				this.px1D_to_3D.get( px1D ).add( new PixelGroupTime( i, wStart, wEnd ) );
-//			}
-//
-//
-//			all_dists = read_dists( is );
-//
-//			int lab = -1;
-//			is.read( buff_i );
-//			int Ncomps = byte_to_int( buff_i );
-//			for ( int k = 0; k < Ncomps; ++k )
-//			{
-//				is.read( buff_i );
-//				lab = byte_to_int( buff_i );
-//				this.dists.put(lab, new IntHashMap<> () );
-//
-//				is.read( buff_i );
-//				Nelt = byte_to_int( buff_i );
-//				for ( int i = 0; i < Nelt; ++i )
-//				{
-//					is.read( buff_i );
-//					int px_s3D = byte_to_int( buff_i );
-//					this.dists.get( lab ).put( px_s3D, new IntHashMap<> ( 2 ) );
-//
-//					is.read( buff_i );
-//					int Nelt2 = byte_to_int( buff_i );
-//					for ( int j = 0; j < Nelt2; ++j )
-//					{
-//						is.read( buff_i );
-//						int px_s3D2 = byte_to_int( buff_i );
-//
-//						is.read( buff_i );
-//						int Nelt3 = byte_to_int( buff_i );
-//						for ( int l = 0; l < Nelt3; ++l )
-//						{
-//							is.read( buff_i );
-//							int didx = byte_to_int( buff_i );
-//							is.read( buff_i );
-//							int wStart = byte_to_int( buff_i );
-//							is.read( buff_i );
-//							int wEnd = byte_to_int( buff_i );
-//
-//							//System.out.println(String.format("%d %d %d %d %d %d", comp, px_s, px_d, wStart, wEnd, dw));
-//							CompTime ct = new CompTime( all_dists[didx], this.win_cents.get( wStart ) - dw,
-//									this.win_cents.get( wEnd ) + dw );
-//							if ( ! this.elts_f.containsKey( ct ) )
-//							{
-//								int N = this.elts_f.size() - 1;
-//								this.elts_f.put( ct, N );
-//								this.elts_r.put( N, ct );
-//							}
-//
-//							if ( ! this.dists.get( lab ). get( px_s3D ).containsKey( px_s3D2 ) )
-//								this.dists.get( lab ).get( px_s3D ).put( px_s3D2, new ArrayList<> ());
-//
-//							this.dists.get( lab ).get( px_s3D ).get( px_s3D2 ).add( this.elts_f.get( ct ) );
-//						}
-//					}
-//				}
-//			}
 
 			assert( is.read(buff_i) == -1 );
 		}
@@ -332,9 +387,9 @@ public class ComponentDistancesTime
 		}
 	}
 
-	public ComponentDistancesTime( final String fname, final ImagePlus compImg, int w, int h, double dt, double pxsize ) throws FileNotFoundException
+	public ComponentDistancesTime( final String fname, final ImagePlus compImg, int w, int h, double pxsize ) throws FileNotFoundException
 	{
-		this.dists = new IntHashMap<> ();
+		this.dists = new Distances ();
 		this.is2D = false;
 		this.w = w;
 		this.h = h;
@@ -343,9 +398,9 @@ public class ComponentDistancesTime
 		this.spotComps = new IntHashMap<> ();
 
 		this.elts_f = new HashMap<CompTime, Integer> ();
-		this.elts_r = new HashMap<Integer, CompTime> ();
+		this.elts_r = new ArrayList<> ();
 
-		this.read_binary_spts( fname, dt );
+		this.read_binary_spts( fname );
 	}
 
 	public ArrayList<Integer> getWindowIdxs ( int frame )
@@ -358,10 +413,10 @@ public class ComponentDistancesTime
 		}
 
 		ArrayList<Integer> res = new ArrayList<> ();
-		for ( int i = 0; i < this.win_cents.size(); ++i )
+		for ( int i = 0; i < this.win_starts.size(); ++i )
 		{
-			if ( frame >= this.win_cents.get( i ) - this.wdur / 2 &&
-				 frame <= this.win_cents.get( i ) + this.wdur / 2 )
+			if ( frame >= this.win_starts.get( i ) &&
+				 frame < this.win_starts.get( i ) + this.wdur )
 				res.add( Integer.valueOf( i ) );
 		}
 		return res;
@@ -383,17 +438,17 @@ public class ComponentDistancesTime
 
 			this.spotComps.put( s.ID(), comps );
 		}
-		System.out.println(String.format("comp size = %d", spotComps.size()));
+		System.out.println( String.format( "comp size = %d", spotComps.size() ) );
 	}
 
 	public void clear_spots_comps()
 	{
-		System.out.println("Clearing spots components cache");
+		System.out.println( "Clearing spots components cache" );
 		this.spotComps.clear();
-		System.out.println(this.spotComps.size());
+		System.out.println( this.spotComps.size() );
 	}
 
-	public float dist( int px1, int px2, int comp, int winIdx )
+	public float dist( int px1, int px2, int comp, int win )
 	{
 		if ( px1 > px2 )
 		{
@@ -402,17 +457,21 @@ public class ComponentDistancesTime
 			px2 = tmp;
 		}
 
-		if ( !this.dists.containsKey( comp ) || !this.dists.get( comp ).containsKey( px1 ) ||
-			 !this.dists.get( comp ).get( px1 ).containsKey( px2 ) )
+		int i = this.dists.getCompIdx( comp );
+		if ( i < 0 || i >= this.dists.dists.length )
+			return Float.MAX_VALUE;
+		int j = this.dists.getCompSrcIdx( i,  px1 );
+		if ( j < 0 || j >= this.dists.dists[i].length )
+			return Float.MAX_VALUE;
+		int k = this.dists.getCompDstIdx( i, j, px2 );
+		if ( k < 0 || k >= this.dists.dists[i][j].length )
+			return Float.MAX_VALUE;
+		int l = this.getWinIdx( i, j, k, win );
+		if ( l < 0 || l >= this.dists.dists[i][j][k].length )
 			return Float.MAX_VALUE;
 
-		return this.elts_r.get( this.dists.get( comp ).get( px1 ).get( px2 ).get( winIdx ) ).dist;
+		return this.elts_r.get( this.dists.dists[i][j][k][l] ).dist;
 	}
-
-//	public IntHashMap< IntHashMap< Integer > > dists()
-//	{
-//		return this.dists;
-//	}
 
 	public int w()
 	{
@@ -429,9 +488,14 @@ public class ComponentDistancesTime
 		return this.pxsize;
 	}
 
-	public ArrayList<Integer> win_cents()
+	public Distances dists()
 	{
-		return this.win_cents;
+		return this.dists;
+	}
+
+	public ArrayList<Integer> win_starts()
+	{
+		return this.win_starts;
 	}
 
 	public IntHashMap< ArrayList<SpotComp > > spotComps()
@@ -450,6 +514,5 @@ public class ComponentDistancesTime
 	public int px1D( int[] p )
 	{
 		return p[1] * this.w + p[0];
-		//return p[0] * this.h + p[1];
 	}
 }
--- src/main/java/fiji/plugin/trackmate/tracking/sparselap/costfunction/ReachableDistCostFunctionTime.java
+++ src/main/java/fiji/plugin/trackmate/tracking/sparselap/costfunction/ReachableDistCostFunctionTime.java
@@ -7,7 +7,6 @@ public class ReachableDistCostFunctionTime implements CostFunction< Spot, Spot >
 {
 	protected final ComponentDistancesTime cdists;
 
-	//CHECK W and H here
 	protected static ArrayList<int[]> neighbors( int[] p, int w, int h )
 	{
 		int[][] nhs = {{-1, 1}, {0, 1}, {1, 1}, {-1, 0}, {1, 0}, {-1, -1}, {0, -1}, {1, -1}};
@@ -88,19 +87,12 @@ public class ReachableDistCostFunctionTime implements CostFunction< Spot, Spot >
 						continue;
 					}
 
-					int pxs3D = this.cdists.map_1D_to_3D( pxs, curWin );
-					int pxd3D = this.cdists.map_1D_to_3D( pxd, curWin );
-
-					if ( pxs3D == -1 || pxd3D == -1 )
-						continue;
-
-					int px1 = Math.min( pxs3D, pxd3D );
-					int px2 = Math.max( pxs3D, pxd3D );
+					int px1 = Math.min( pxs, pxd );
+					int px2 = Math.max( pxs, pxd );
 
 					d1 = Math.pow( Math.sqrt( ComponentDistancesTime.sq_dist_to_px( poss, src_px, cdists.pxsize() ) ) +
 								   this.cdists.dist( px1, px2, sComp, curWin ) +
 								   Math.sqrt( ComponentDistancesTime.sq_dist_to_px( post, dst_px, cdists.pxsize() ) ), 2 );
-
 					final double d2 = ( d1 == 0 ) ? Double.MIN_NORMAL : d1;
 
 					if ( d2 < best_dist )
@@ -108,118 +100,6 @@ public class ReachableDistCostFunctionTime implements CostFunction< Spot, Spot >
 				}
 			}
 		}
-
-		assert( best_dist >= base_dist );
-		return best_dist;
-	}
-
-	public double linkingCostLog( final Spot source, final Spot target )
-	{
-		int sFrame = source.getFeature( "FRAME" ).intValue();
-		int tFrame = target.getFeature( "FRAME" ).intValue();
-		int[] src_px2D = { source.getFeature( "PX_X" ).intValue(), source.getFeature( "PX_Y" ).intValue() };
-		int[] dst_px2D = { target.getFeature( "PX_X" ).intValue(), target.getFeature( "PX_Y" ).intValue() };
-		int sComp = this.cdists.getComponent( sFrame, src_px2D );
-		int tComp = this.cdists.getComponent( tFrame, dst_px2D );
-
-		int s1D = cdists.px1D( src_px2D );
-		int d1D = cdists.px1D( dst_px2D );
-		System.out.println(String.format("source: frame=%d pos=(%d %d) comp=%d 1D=%d", sFrame, src_px2D[0], src_px2D[1], sComp, s1D));
-		System.out.println(String.format("target: frame=%d pos=(%d %d) comp=%d 1D=%d", tFrame, dst_px2D[0], dst_px2D[1], tComp, d1D));
-
-		if ( sComp != tComp )
-		{
-			System.out.println("  sComp != tComp");
-			return Double.POSITIVE_INFINITY;
-		}
-
-		double d1 = source.squareDistanceTo( target );
-		System.out.println(String.format("  edist=%g", d1));
-
-		final double base_dist = d1;
-
-		if ( d1 > 25 )
-			return d1;
-
-		ArrayList<Integer> winsInter = this.intersectWins( this.cdists.getWindowIdxs( sFrame ),
-														   this.cdists.getWindowIdxs( tFrame ) );
-
-		if ( winsInter.isEmpty() )
-		{
-			System.out.println("  No intersecting window");
-			return Double.POSITIVE_INFINITY;
-		}
-
-
-		double[] poss = { source.getFeature( "POSITION_X" ), source.getFeature( "POSITION_Y" ) };
-		double[] post = { target.getFeature( "POSITION_X" ), target.getFeature( "POSITION_Y" ) };
-
-		ArrayList<int[]> src_pxs = ReachableDistCostFunctionTime.neighbors(
-				src_px2D, this.cdists.w(), this.cdists.h() );
-
-		ArrayList<int[]> dst_pxs = ReachableDistCostFunctionTime.neighbors(
-				dst_px2D, this.cdists.w(), this.cdists.h() );
-
-		double best_dist = Float.MAX_VALUE;
-		for ( int curWin: winsInter )
-		{
-			System.out.println(String.format("  curWin=%d", curWin));
-			for ( int[] src_px: src_pxs )
-			{
-				int pxs = cdists.px1D( src_px );
-
-				System.out.println(String.format("    src=(%d %d) 1D=%d", src_px[0], src_px[1], pxs));
-
-				for ( int[] dst_px: dst_pxs )
-				{
-					int pxd = cdists.px1D( dst_px );
-
-					System.out.println(String.format("    dst=(%d %d) 1D=%d", dst_px[0], dst_px[1], pxd));
-
-					if ( pxs == pxd )
-					{
-						d1 = source.squareDistanceTo( target );
-						final double d2 = ( d1 == 0 ) ? Double.MIN_NORMAL : d1;
-
-						System.out.println(String.format("    Same px using edist=%g", d2));
-
-						if ( d2 < best_dist )
-							best_dist = d2;
-						continue;
-					}
-
-					int pxs3D = this.cdists.map_1D_to_3D( pxs, curWin );
-					int pxd3D = this.cdists.map_1D_to_3D( pxd, curWin );
-
-					System.out.println(String.format("    pxs3D: src=%d dst=%d", pxs3D, pxd3D));
-
-					if ( pxs3D == -1 || pxd3D == -1 )
-						continue;
-
-					int px1 = Math.min( pxs3D, pxd3D );
-					int px2 = Math.max( pxs3D, pxd3D );
-
-					System.out.println(String.format("    ordering: px1=%d px2=%d", px1, px2));
-
-					d1 = Math.pow( Math.sqrt( ComponentDistancesTime.sq_dist_to_px( poss, src_px, cdists.pxsize() ) ) +
-								   this.cdists.dist( px1, px2, sComp, curWin ) +
-								   Math.sqrt( ComponentDistancesTime.sq_dist_to_px( post, dst_px, cdists.pxsize() ) ), 2 );
-
-					System.out.println(String.format("    edist_src=%g cdist=%g edist_dst=%g sum_sq=%g",
-							Math.sqrt( ComponentDistancesTime.sq_dist_to_px( poss, src_px, cdists.pxsize() ) ),
-							this.cdists.dist( px1, px2, sComp, curWin ),
-							Math.sqrt( ComponentDistancesTime.sq_dist_to_px( post, dst_px, cdists.pxsize() ) ),
-							d1));
-
-					final double d2 = ( d1 == 0 ) ? Double.MIN_NORMAL : d1;
-
-					if ( d2 < best_dist )
-						best_dist = d2;
-				}
-			}
-		}
-
-		System.out.println(String.format("  bestDist=%g", best_dist));
 
 		assert( best_dist >= base_dist );
 		return best_dist;
